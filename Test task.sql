3016c8203bb94587aeb027489e4b3a95b9d05eb1
IrinaEvseeva <87815328+IrinaEvseeva@users.noreply.github.com>
2021-07-22 19:20:49 +0300

    Create Test task.sql
    Тестовое 1. SQL

---
+++ Test task.sql
@@ -0,0 +1,18 @@
+Тестовое задание 1.
+
+Решение:
+
+SELECT
+
+DISTINCT task_info.issue_key AS number,
+
+task_info.name AS name
+
+FROM task_status
+
+INNER JOIN task_info ON task_info.issue_key = task_status.issue_key
+
+WHERE 
+
+type = 'Bug' AND status = 'in progress' AND start_time BETWEEN '2021-04-01' AND '2021-04-30';
+
